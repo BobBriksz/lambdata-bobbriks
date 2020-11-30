c5c8c7c73c0ede135e7e87a3ea4f7911fedb2958
BobBriksz <robert.briksza@gmail.com>
2020-11-30 14:01:16 -0500

    created setupfile

---
+++ lambdata/__init__.py
@@ -0,0 +1,9 @@
+""""Lambdata - a collection """""
+
+
+import pandas as import pd
+import numpy as np 
+
+def df_cleaner(df):
+    """"clean a df of nulls"""""
+    #TODO -implement 
---
+++ setup.py
@@ -0,0 +1,6 @@
+    install_requires=REQUIRED,
+    classifiers=[
+        "Programming language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+            ]
+)
