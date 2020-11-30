ac497f35fc16cd69fd514c1a8641b15b24bc1b72
BobBriksz <robert.briksza@gmail.com>
2020-11-30 14:45:04 -0500

    pipfile and lock file

--- setup.py
+++ setup.py
@@ -1,3 +1,24 @@
+""""lambdata- collaction of data science functions"""""
+
+import setuptools #available with standard library
+
+REQUIRED = [
+    "numpy",
+    "pandas"
+]
+
+with open("readme.md", "r") as fh:
+        LONG_DESCRIPTION = fh.read()
+
+
+setuptools.setup(
+    name='lambdata-bobbriks',
+    version='0.0.1',
+    description=LONG_DESCRIPTION,
+    long_description_content='text/markdown',
+    url ="https://github.com/BobBriksz/lambdata-bobbriks",
+    packages= setuptools.find_packages #how we want to find our require packages
+    python_requires=">=3.6", #what versions of python we are compatible with
     install_requires=REQUIRED,
     classifiers=[
         "Programming language :: Python :: 3",
